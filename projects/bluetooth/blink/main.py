920130fa3fd8f25800260508c13914fc41ab0e4e
Omar Valdez <80359343+ValdezFOmar@users.noreply.github.com>
2023-10-25 14:26:37 -0700

    Clean up bluetooth main.py

--- projects/bluetooth/blink/main.py
+++ projects/bluetooth/blink/main.py
@@ -7,9 +7,9 @@ pico_led = Pin("LED", Pin.OUT)
 
 def on_rx(data: bytes):
     parsed_data = data.decode().strip()
-    print("Data received: ", parsed_data)  # Print the received data
+    print("Data received:", parsed_data)
 
-    if parsed_data == "on":  # Check if the received data is "toggle"
+    if parsed_data == "on":
         pico_led.on()
     elif parsed_data == "off":
         pico_led.off()
@@ -21,9 +21,8 @@ def main():
     ble = bluetooth.BLE()
     sp = BLESimplePeripheral(ble, "pico-w-board")
 
-    # Start an infinite loop
     while True:
-        if sp.is_connected():  # Check if a BLE connection is established
+        if sp.is_connected():
             sp.on_write(on_rx)
 
 
