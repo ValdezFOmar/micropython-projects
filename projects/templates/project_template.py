905f1a250b74707f4ea5953de427aa7f3bfab49a
Omar Valdez <80359343+ValdezFOmar@users.noreply.github.com>
2023-10-31 17:20:40 -0700

    Adds template for new projects

---
+++ projects/templates/project_template.py
@@ -0,0 +1,46 @@
+################################################################################
+# Nombre del Archivo: main.py
+# Autor:             Omar Antonio Valdez Fuentes
+# Correo:            l20212434@tectijuana.edu.mx
+# Fecha:             [Fecha]
+# Institución:       Tecnológico Nacional de México (TECNM) - Campus ITT
+# Curso:             Sistemas Programables
+#
+# Objetivo:
+# Este programa está diseñado para [proporcionar una breve descripción del objetivo del programa].
+#
+# Historial de Revisiones:
+# [Fecha]        [Tu Nombre] - Creado
+# [Fecha]        [Tu Nombre] - Actualizado para añadir [característica/corrección]
+#
+# Enlace a GitHub Repository ó GIST:
+# https://github.com/ValdezFOmar/micropython-projects/
+#
+# Enlace a Wokwi :
+# [URL del Wokwi]
+#
+# Licencia:
+# Este programa es software libre y puede ser redistribuido y/o modificado bajo los términos de la Licencia Pública General GNU
+# como está publicado por la Free Software Foundation, ya sea la versión 3 de la Licencia, o (a tu elección) cualquier versión posterior.
+#
+# Este programa se distribuye con la esperanza de que sea útil, pero SIN GARANTÍA ALGUNA; incluso sin la garantía implícita de
+# COMERCIALIZACIÓN o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulte la Licencia Pública General GNU para obtener más detalles.
+#
+# Deberías haber recibido una copia de la Licencia Pública General GNU junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
+#
+################################################################################
+# Importación de módulos necesarios
+# import ...
+
+# Definición de funciones
+# def ...
+
+# Código principal
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
